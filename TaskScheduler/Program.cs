02b991f726f1432ec55ef6e910695499753e7577
Ran Shahi <randeepshahi@gmail.com>
2026-02-10 16:55:09 -0800

    work in progress.

--- TaskScheduler/Program.cs
+++ TaskScheduler/Program.cs
@@ -1,8 +1,6 @@
 ﻿/*
 
-You want to create a task scheduler. This task scheduler will be reponsible for the following
-- keep track of which computers are currently running tasks
-- assign a new task to a free computer
+Create a task scheduler
 
 */
 
@@ -13,16 +11,52 @@ Console.WriteLine("Hello, World!");
 
 public class TaskScheduler
 {
-    private int totalNumberOfComputers;
-    TaskScheduler(int numberOfComputers)
+    private readonly int totalCores;
+    private PriorityQueue<int, int> freeCores;
+    private HashSet<int> usedCores;
+    private int errorNoFreeCores = -1;
+    private Dictionary<int, int> coreToExecutionTimeDict;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="numberOfCores"></param>
+    TaskScheduler(int numberOfCores)
     {
-        
+        this.totalCores = numberOfCores;
+        this.usedCores = new HashSet<int>();
+        this.freeCores = new PriorityQueue<int, int>();
+        this.coreToExecutionTimeDict = new Dictionary<int, int>();
+
+        for(int i = 0; i < numberOfCores; i++)
+        {
+            freeCores.Enqueue(i, 0);
+            coreToExecutionTimeDict[i] = 0;
+        }
     }
 
-    bool AssignJobToComputer()
+    /// <summary>
+    /// Assigns a job to a free core.
+    /// </summary>
+    /// <returns>-1 if no free core.</returns>
+    int AssignJob()
     {
-        
+        if(freeCores.Count > 0)
+        {
+            int core = freeCores.Dequeue();
+            usedCores.Add(core);
+            return core;
+        }
+        return errorNoFreeCores;
     }
 
-    bool FreeComputer
+    void ReturnCore(int coreNumber)
+    {
+        // prevents adding the same core twice
+        if(usedCores.Contains(coreNumber))
+        {
+            freeCores.Enqueue(coreNumber);
+            usedCores.Remove(coreNumber);
+        }
+    }
 }
